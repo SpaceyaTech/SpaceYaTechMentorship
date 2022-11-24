3bdd2bcbcf3c09f74072af4123d7371e2e2c7ae6
IanMcbull <ianmugenya@gmail.com>
2022-11-24 12:59:28 +0300

    Made more changes to the UI and functionality

--- JavaScript/DOM/Tip Calculator/app.js
+++ JavaScript/DOM/Tip Calculator/app.js
@@ -3,11 +3,10 @@ const buttons = Array.from(document.querySelectorAll('.tip-btn'))
 
 // Lisening for a click event on all the buttons
 buttons.forEach(button => {
-        button.addEventListener('click', function(){
+        button.addEventListener('click', function(){ 
             const tipPercentage = parseInt(button.dataset.percentage) / 100;
             const billAmount = parseInt(document.querySelector('#bill-amount').value);
-            const numberOfPeople = parseInt(document.querySelector('#number-of-people').value);
-            
+            const numberOfPeople = parseInt(document.querySelector('#number-of-people').value);          
             calculateTip(tipPercentage,billAmount,numberOfPeople);
         })    
 })
@@ -25,7 +24,6 @@ function validateInput(){
         // This is a ternary operator
         // It's a shorthand for an if/else statement
         // "condition" ? "if true" : "if false"
-       
         !billAmount > 0 ? document.querySelector('#bill-amount').classList.add('input-error') : document.querySelector('#bill-amount').classList.remove('input-error');
         !numberOfPeople > 0 ? document.querySelector('#number-of-people').classList.add('input-error') : document.querySelector('#number-of-people').classList.remove('input-error');
         return false;
@@ -49,17 +47,32 @@ function calculateTip(tipPercentage,billAmount,numberOfPeople) {
 
 const customInput = document.querySelector('#custom-tip')
 customInput.addEventListener('keyup',(e)=>{
-    if(e.key === 'Enter'){
-     console.log("Enter key has been pressed")
-     if(customInput){
-        const tipPercentage = parseInt(customInput.value) / 100;
-        const billAmount = parseInt(document.querySelector('#bill-amount').value);
-        const numberOfPeople = parseInt(document.querySelector('#number-of-people').value);
-        calculateTip(tipPercentage,billAmount,numberOfPeople);
+    if(!customInput.value){
+      customInput.classList.add('input-error')
+    }else{
+        customInput.classList.remove('input-error')
+        if(e.key === 'Enter' || e.key){
+            if(customInput){
+               const tipPercentage = parseInt(customInput.value) / 100;
+               const billAmount = parseInt(document.querySelector('#bill-amount').value);
+               const numberOfPeople = parseInt(document.querySelector('#number-of-people').value);
+               calculateTip(tipPercentage,billAmount,numberOfPeople);
+           }
+        }
     }
- }
     
 })
+
+// Add event listener for the reset button
+document.querySelector(".btn-reset").addEventListener("click",()=>{
+    document.querySelector('#tip-amount').innerHTML = '$0.00';
+    // Change the innerHTML of the total amount
+    document.querySelector('#total-amount').innerHTML = '$0.00';
+    document.querySelector('#bill-amount').value = ''
+    document.querySelector('#number-of-people').value = ''
+})
+
+
 
 
 
