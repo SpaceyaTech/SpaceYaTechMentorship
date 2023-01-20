08193a89f1d890a5849cfd1dd41ae901ce6f8985
IanMcbull <ianmugenya@gmail.com>
2023-01-20 08:21:46 +0300

    syntax edits

--- JavaScript/OOP/Book List Project/app.js
+++ JavaScript/OOP/Book List Project/app.js
@@ -8,7 +8,7 @@ function Book(title,author,isbn){
 }
 
 // UI constructor
-function UIController(){}
+function UIController(){};
 
 
 UIController.prototype.addBookToUI = function(book){
@@ -16,12 +16,12 @@ UIController.prototype.addBookToUI = function(book){
  const tr = document.createElement('tr');
  tr.innerHTML = 
  `<td>${book.title}</td>
- <td>${book.author}</td>
- <td>${book.isbn}</td>
- <td><a href="#"><i class="fa-solid fa-trash"></i></a></td>
- `
- tableBody.appendChild(tr)
-}
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><button type="button" class="btn btn-light btn-sm"><i class="fa-solid fa-trash text-danger"></i></button></td>
+ `;
+ tableBody.appendChild(tr);
+};
 
 UIController.prototype.alerts = function(alertType){
     const alertContainer = document.querySelector('#alert')
@@ -29,26 +29,26 @@ UIController.prototype.alerts = function(alertType){
      alertContainer.innerHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
      <strong>Book added successfully</strong> 
      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-   </div>`
+   </div>`;
     }else{
         alertContainer.innerHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
      <strong>You need to provide all the fields</strong> 
      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-   </div>`
+   </div>`;
         
     }
-}
+};
 
 // Clear Fields Method
 UIController.prototype.clearFields = function(){
     
-    document.querySelector('#form').reset()
+    document.querySelector('#form').reset();
     
     // document.querySelector('#title').value = '';
     // document.querySelector('#author').value = '';
     // document.querySelector('#ISBN').value = '';
 
-}
+};
 
 
 
@@ -59,16 +59,14 @@ formElement.addEventListener('submit',function(e){
   const title = document.querySelector('#title').value;  
   const author = document.querySelector('#author').value; 
   const isbn = document.querySelector('#ISBN').value; 
-  const ui = new UIController()
+  const ui = new UIController();
 
   if(!title.trim() || !author.trim() || !isbn.trim()){
-    ui.alerts('error')
+    ui.alerts('error');
   }else{
    const book = new Book(title, author, isbn);
-   ui.alerts('success')
-   ui.addBookToUI(book)
+   ui.alerts('success');
+   ui.addBookToUI(book);
    ui.clearFields();
-
   }
-
-})
+});
